18d3afe7da31c32637973d9b0d5cef5159ed7102
aman-123-wq <amanjv2005@gmail.com>
2025-10-10 21:31:56 +0530

    update firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -2,17 +2,21 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyBp6Sm50Dd1vYBlgDm_Co2X9AH4_XbIK9w",
+    authDomain: "mediconnect-hms.firebaseapp.com",
+    projectId: "mediconnect-hms",
+    storageBucket: "mediconnect-hms.firebasestorage.app",
+    messagingSenderId: "369434106177",
+    appId: "1:369434106177:web:b9baa6194ee921eaaa69c0",
+    measurementId: "G-3V2E65XCBY"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
