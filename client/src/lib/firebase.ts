eac6efee37acba859b1f6bd0ae8d1b7e9e01e462
aman-123-wq <amanjv2005@gmail.com>
2025-10-12 12:31:38 +0530

    connection

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -2,21 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBp6Sm50Dd1vYBlgDm_Co2X9AH4_XbIK9w",
-    authDomain: "mediconnect-hms.firebaseapp.com",
-    projectId: "mediconnect-hms",
-    storageBucket: "mediconnect-hms.firebasestorage.app",
-    messagingSenderId: "369434106177",
-    appId: "1:369434106177:web:b9baa6194ee921eaaa69c0",
-    measurementId: "G-3V2E65XCBY"
+  apiKey: "AIzaSyBp6Sm50Dd1vYBlgDm_Co2X9AH4_XbIK9w",
+  authDomain: "mediconnect-hms.firebaseapp.com",
+  projectId: "mediconnect-hms",
+  storageBucket: "mediconnect-hms.firebasestorage.app",
+  messagingSenderId: "369434106177",
+  appId: "1:369434106177:web:b9baa6194ee921eaaa69c0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
